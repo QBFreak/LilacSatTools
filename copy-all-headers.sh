17956ee28f3b1e895fc66102a8fe4a8a6072f161
Jason Hill <qbfreak@qbreak.net>
2017-08-17 12:57:35 -0400

    Fixed python path, changed extension of shell scripts

--- copy-all-headers.sh
+++ copy-all-headers.sh
@@ -3,5 +3,5 @@ for i in *.jpg; do
   [ -f "$i" ] || break
   echo
   echo @@ $i
-  /c/python27/python copyheader.py --image-file=$i --header-file=$i.hdr || rm $i.hdr
+  python copyheader.py --image-file=$i --header-file=$i.hdr || rm $i.hdr
 done
